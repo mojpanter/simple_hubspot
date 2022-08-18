3fe7327a8c6e554aded08821a9631663e2a591fa
Jakub Motycka <jakubonty@centrum.cz>
2022-08-18 10:31:08 +0200

    chore: private apps authorization

--- lib/simple_hubspot/api_client.rb
+++ lib/simple_hubspot/api_client.rb
@@ -3,14 +3,14 @@ module SimpleHubspot
     class << self
 
       def do_post(path = nil, params = {}, headers = {})
-        response = RestClient.post "#{SimpleHubspot.configuration.api_base}#{path}#{add_apikey}", params.to_json, { content_type: :json }
+        response = RestClient.post "#{SimpleHubspot.configuration.api_base}#{path}", params.to_json, { Authorization: "Bearer #{SimpleHubspot.configuration.hapikey}", content_type: :json }
         response_success response.body
       rescue RestClient::BadRequest => e
         response_fail e.response.body
       end
 
       def do_get(path = nil, params = {}, headers = {})
-        response = RestClient.get "#{SimpleHubspot.configuration.api_base}#{path}#{add_apikey}"
+        response = RestClient.get "#{SimpleHubspot.configuration.api_base}#{path}", { Authorization: "Bearer #{SimpleHubspot.configuration.hapikey}" }
         json = JSON.parse(response.body, symbolize_names: true)
         json.merge(success: true)
       rescue RestClient::BadRequest => e
